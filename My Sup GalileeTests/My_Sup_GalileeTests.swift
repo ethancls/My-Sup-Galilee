2e67a6bdbebe38772d0544e8019ab8a7a591397f
ethancls <ethancls@icloud.com>
2024-10-26 18:55:42 +0200

    Initial Commit

---
+++ My Sup GalileeTests/My_Sup_GalileeTests.swift
@@ -0,0 +1,17 @@
+//
+//  My_Sup_GalileeTests.swift
+//  My Sup GalileeTests
+//
+//  Created by Ethan Nicolas on 26/10/2024.
+//
+
+import Testing
+@testable import My_Sup_Galilee
+
+struct My_Sup_GalileeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
